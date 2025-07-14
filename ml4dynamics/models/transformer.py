c60e1d820497ffd730c45c974913162f4ad75dea
Sassan <sassanbhanji@icloud.com>
2025-07-14 16:33:12 +0800

    Transformer 1.2

---
+++ ml4dynamics/models/transformer.py
@@ -0,0 +1,125 @@
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+import numpy as np
+import math
+
+class PositionalEncoding(nn.Module):
+    d_model: int
+    max_len: int = 5000
+
+    def setup(self):
+        pe = np.zeros((self.max_len, self.d_model))
+        position = np.arange(0, self.max_len, dtype=np.float32)[:, None]
+        div_term = np.exp(np.arange(0, self.d_model, 2) * (-math.log(10000.0) / self.d_model))
+        pe[:, 0::2] = np.sin(position * div_term)
+        pe[:, 1::2] = np.cos(position * div_term)
+        pe = pe[None]
+        self.pe = jax.device_put(pe)
+
+    def __call__(self, x):
+        x = x + self.pe[:, :x.shape[1]]
+        return x
+
+class MultiheadAttention(nn.Module):
+    embed_dim: int
+    num_heads: int
+
+    def setup(self):
+        self.qkv_proj = nn.Dense(3 * self.embed_dim)
+        self.o_proj = nn.Dense(self.embed_dim)
+
+    def __call__(self, x, mask=None):
+        batch_size, seq_length, embed_dim = x.shape
+        qkv = self.qkv_proj(x)
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, -1)
+        qkv = qkv.transpose(0, 2, 1, 3)
+        q, k, v = jnp.array_split(qkv, 3, axis=-1)
+        d_k = q.shape[-1]
+        attn_logits = jnp.matmul(q, jnp.swapaxes(k, -2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attn_logits = jnp.where(mask == 0, -9e15, attn_logits)
+        attention = nn.softmax(attn_logits, axis=-1)
+        values = jnp.matmul(attention, v)
+        values = values.transpose(0, 2, 1, 3).reshape(batch_size, seq_length, embed_dim)
+        o = self.o_proj(values)
+        return o, attention
+
+class EncoderBlock(nn.Module):
+    input_dim: int
+    num_heads: int
+    dim_feedforward: int
+    dropout_prob: float
+
+    def setup(self):
+        self.self_attn = MultiheadAttention(embed_dim=self.input_dim, num_heads=self.num_heads)
+        self.linear1 = nn.Dense(self.dim_feedforward)
+        self.linear2 = nn.Dense(self.input_dim)
+        self.norm1 = nn.LayerNorm()
+        self.norm2 = nn.LayerNorm()
+        self.dropout = nn.Dropout(self.dropout_prob)
+
+    def __call__(self, x, mask=None, train=True):
+        attn_out, _ = self.self_attn(x, mask=mask)
+        x = x + self.dropout(attn_out, deterministic=not train)
+        x = self.norm1(x)
+        linear_out = nn.relu(self.linear1(x))
+        linear_out = self.linear2(linear_out)
+        x = x + self.dropout(linear_out, deterministic=not train)
+        x = self.norm2(x)
+        return x
+
+class TransformerEncoder(nn.Module):
+    num_layers: int
+    input_dim: int
+    num_heads: int
+    dim_feedforward: int
+    dropout_prob: float
+
+    def setup(self):
+        self.layers = [EncoderBlock(self.input_dim, self.num_heads, self.dim_feedforward, self.dropout_prob) for _ in range(self.num_layers)]
+
+    def __call__(self, x, mask=None, train=True):
+        for l in self.layers:
+            x = l(x, mask=mask, train=train)
+        return x
+
+class Transformer2D(nn.Module):
+    model_dim: int
+    num_classes: int
+    num_heads: int
+    num_layers: int
+    dropout_prob: float = 0.0
+    input_dropout_prob: float = 0.0
+
+    def setup(self):
+        self.input_dropout = nn.Dropout(self.input_dropout_prob)
+        self.input_layer = nn.Dense(self.model_dim)
+        self.positional_encoding = PositionalEncoding(self.model_dim)
+        self.transformer = TransformerEncoder(
+            num_layers=self.num_layers,
+            input_dim=self.model_dim,
+            dim_feedforward=2 * self.model_dim,
+            num_heads=self.num_heads,
+            dropout_prob=self.dropout_prob
+        )
+        self.dense1 = nn.Dense(self.model_dim)
+        self.norm = nn.LayerNorm()
+        self.dropout = nn.Dropout(self.dropout_prob)
+        self.dense2 = nn.Dense(self.num_classes)
+
+    def __call__(self, x, mask=None, add_positional_encoding=True, train=True):
+        B, H, W, C = x.shape
+        x = x.reshape(B, H * W, C)
+        x = self.input_dropout(x, deterministic=not train)
+        x = self.input_layer(x)
+        if add_positional_encoding:
+            x = self.positional_encoding(x)
+        x = self.transformer(x, mask=mask, train=train)
+        x = self.dense1(x)
+        x = self.norm(x)
+        x = nn.relu(x)
+        x = self.dropout(x, deterministic=not train)
+        x = self.dense2(x)
+        x = x.reshape(B, H, W, self.num_classes)
+        return x
